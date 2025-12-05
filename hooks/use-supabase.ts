f4d1b74acfa0332b9cc1f0944c63cde1c3124f48
Cleo <cleopatrathequeenofcats@gmail.com>
2025-12-05 17:25:12 +0200

    fix: restore working site after SonarCloud fixes

--- hooks/use-supabase.ts
+++ hooks/use-supabase.ts
@@ -1,9 +1,4 @@
 'use client'
-import { useEffect, useState } from 'react'
 import { getBrowserSupabaseClient } from '@/lib/supabase-client'
 
-export const useSupabase = () => {
-  const [client, setClient] = useState<any>(null)
-  useEffect(() => setClient(getBrowserSupabaseClient()), [])
-  return client
-}
+export const useSupabase = () => getBrowserSupabaseClient()
