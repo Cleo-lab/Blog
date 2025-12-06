eee6e5b1e5aa16a5ab59be85358615419e11549a
Cleo <cleopatrathequeenofcats@gmail.com>
2025-12-06 18:40:22 +0200

    fix:contain: content

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+// next.config.js
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false, // не пытаемся открыть браузер
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -8,4 +17,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default bundleAnalyzer(nextConfig)
