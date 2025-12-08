7e47cb59a10ecbafd9d694868af7a27571d67beb
Cleo <cleopatrathequeenofcats@gmail.com>
2025-12-08 12:34:22 +0200

    add sitemap and robots.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://yurieblog.vercel.app',          // замени на свой домен
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://yurieblog.vercel.app/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://yurieblog.vercel.app/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
