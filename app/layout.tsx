c00cff7ff567a81b847e648812e5edebf6f6a40a
Cleo <cleopatrathequeenofcats@gmail.com>
2025-12-09 16:01:47 +0200

    add warning for cookies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from 'next/font/google'
 import Script from 'next/script'
 import './globals.css'
 import { ProvidersWrapper } from '@/components/providers-wrapper'
-import CookieConsent from "react-cookie-consent";
+import CookieConsent from 'react-cookie-consent'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,31 +18,37 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({ children }: { readonly children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  readonly children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={poppins.className}>
         <ProvidersWrapper>{children}</ProvidersWrapper>
 
-        {/* AdSense script for test ads */}
+        {/* AdSense */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           strategy="afterInteractive"
         />
-		<CookieConsent
-  location="bottom"
-  buttonText="I understand"
-  cookieName="mySiteCookieConsent"
-  style={{ background: "#2B373B" }}
-  buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-  expires={150}
->
-  This website uses cookies to enhance the user experience.{" "}
-  <a href="/privacy-policy" style={{ color: "#FFF", textDecoration: "underline" }}>
-    Learn more
-  </a>
-</CookieConsent>
+
+        {/* Cookie-баннер */}
+        <CookieConsent
+          location="bottom"
+          buttonText="I understand"
+          cookieName="mySiteCookieConsent"
+          style={{ background: '#2B373B' }}
+          buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+          expires={150}
+        >
+          This website uses cookies to enhance the user experience.{' '}
+          <a href="/privacy" style={{ color: '#FFF', textDecoration: 'underline' }}>
+            Learn more
+          </a>
+        </CookieConsent>
       </body>
     </html>
   )
